15a423cbfc7788f41c96203505f662b80fd0b1af
Matthias <mattgaleur@gmail.com>
2025-11-27 15:55:56 +0100

    chore: Created class Types

---
+++ apps/shared/src/main/scala/apps/ul2025app68/Types.scala
@@ -0,0 +1,14 @@
+package apps.ul2025app68
+
+import cs214.webapp.UserId
+
+enum Card:
+    case 
+
+case class Hand()
+
+type Board = Map[UserId,Deck]
+
+type DiscardPile = List[Card]
+
+type Pile = List[Card]
