7707502fa4920c6a09771e22abb96e8382666b02
Guillaume Zunion Pommier <guillaume.pommier06@hotmail.com>
2025-11-28 14:33:27 +0100

    refactor: created CardWire and used it in Wire.scala

--- apps/shared/src/main/scala/apps/ul2025app68/Wire.scala
+++ apps/shared/src/main/scala/apps/ul2025app68/Wire.scala
@@ -5,59 +5,62 @@ import cs214.webapp.*
 import cs214.webapp.DecodingException
 
 import scala.util.{Failure, Success, Try}
+import ujson.Value
+import ujson.Obj
 
 object Wire extends AppWire[Event, View]:
-  import Event.*
-  import View.*
-  import ujson.*
+    import Event.*
+    import View.*
+    import ujson.*
 
-  override object eventFormat extends WireFormat[Event]:
-    override def encode(event: Event): Value =
-        event match
-            case Discard(card) => 
-                Obj("type" -> "Discard", "card.ordinal" -> card.ordinal)
-            case PlayCard(card) =>
-                Obj("type" -> "PlayCard", "card.ordinal" -> card.ordinal)
-            case PickCard(isDefaultPile) =>
-                Obj(
-                    "type" -> "PickCard",
-                    "isDefaultPile" -> BooleanWire.encode(isDefaultPile) 
-                )
-        
+    override object eventFormat extends WireFormat[Event]:
+        override def encode(event: Event): Value =
+            event match
+                case Discard(card) => 
+                    Obj("type" -> "Discard", "card" -> CardWire.encode(card))
+                case PlayCard(card) =>
+                    Obj("type" -> "PlayCard", "card" -> CardWire.encode(card))
+                case PickCard(isDefaultPile) =>
+                    Obj(
+                        "type" -> "PickCard",
+                        "isDefaultPile" -> BooleanWire.encode(isDefaultPile) 
+                    )
+            
 
-    override def decode(json: Value): Try[Event] = Try :
-        json("type").str match
-            case "Discard" =>
-                val card : Card = Card.fromOrdinal(json("card.ordinal").num.toInt)
-                Discard(card)
-            case "PlayCard" => 
-                val card : Card = Card.fromOrdinal(json("card.ordinal").num.toInt)
-                PlayCard(card)
-            case "PickCard" =>
-                val isDefaultPile: Boolean = json("isDefaultPile").bool
-                PickCard(isDefaultPile)
-        
+        override def decode(json: Value): Try[Event] = Try :
+            json("type").str match
+                case "Discard" =>
+                    Discard(CardWire.decode(json("card")).get)
+                case "PlayCard" => 
+                    PlayCard(CardWire.decode(json("card")).get)
+                case "PickCard" =>
+                    val isDefaultPile: Boolean = json("isDefaultPile").bool
+                    PickCard(isDefaultPile)
+            
       
 
-  override object viewFormat extends WireFormat[View]:
-    val boardWire = MapWire(StringWire, VectorWire(IntWire))
-    val handWire = SeqWire(IntWire)
-    override def encode(v: View): Value =
-      v match
-        case View(board, hand) => 
-            val boardWithOrdinal = board.map((id, playedHands) => (id, playedHands.map(_.ordinal)))
-            val handWithOrdinal = hand.map(_.ordinal)
-            Obj(
-                "board" -> boardWire.encode(boardWithOrdinal),
-                "hand" -> handWire.encode(handWithOrdinal)
-            )
+    override object viewFormat extends WireFormat[View]:
+        val boardWire = MapWire(StringWire, VectorWire(CardWire))
+        val handWire = SeqWire(CardWire)
+        override def encode(v: View): Value =
+        v match
+            case View(board, hand) => 
+                Obj(
+                    "board" -> boardWire.encode(board),
+                    "hand" -> handWire.encode(hand)
+                )
 
-    override def decode(json: Value): Try[View] = Try:
-        val boardWithOrdinal = boardWire.decode(json("board")).get
-        val board: Board = boardWithOrdinal.map:
-            (id, playedHands) => (id, playedHands.map(Card.fromOrdinal(_)))
-        val handWithOrdinal = handWire.decode(json("hand")).get
-        val hand: Hand = handWithOrdinal.map(Card.fromOrdinal(_)).toVector
-        View(board,hand)  
+        override def decode(json: Value): Try[View] = Try:
+            val board: Board = boardWire.decode(json("board")).get
+            val hand: Hand = handWire.decode(json("hand")).get.toVector
+            View(board,hand)  
             
-        
+
+object CardWire extends WireFormat[Card]:
+    override def encode(t: Card): Value = 
+        IntWire.encode(t.ordinal)
+
+    override def decode(json: Value): Try[Card] = Try:
+        Card.fromOrdinal(
+            IntWire.decode(json).get
+        )
